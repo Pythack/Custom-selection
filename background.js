f8098b9b21f6a9ad960c8a399fcf2b163743aae1
Pythack <59161870+Pythack@users.noreply.github.com>
2023-01-02 19:36:42 +0100

    Add comments to background.js

--- background.js
+++ background.js
@@ -2,18 +2,18 @@ if (typeof browser === "undefined") {
   var browser = chrome;
 }
 
-var localstorage = new Object;
+var localstorage = new Object; // Initialize a local storage
 
-function onError(error) {
+function onError(error) { // Define onError function
     console.log(`Error:${error}`);
 }
 
 async function restoreOptions(tab) {
-  if (/(chrome|about):\/\/.*/.test(tab.url)) {
-    return;
+  if (/(chrome|about):\/\/.*/.test(tab.url)) { // If tab is chrome:// or about://
+    return; // Abort the CSS injection
   }
-  var storage = await browser.storage.local.get();
-  if (localstorage[tab.id]) {
+  var storage = await browser.storage.local.get(); // Get settings
+  if (localstorage[tab.id]) { // If extension already injected CSS in this tab, remove it
     browser.scripting.removeCSS({
       target: {
         tabId: tab.id,
@@ -23,14 +23,14 @@ async function restoreOptions(tab) {
   }
   var css;
   var url = new URL(tab.url);
-  url = url.host;
+  url = url.host; // Get the hostname of the url
   var injected = false;
-  if (storage.customOptions) {
+  if (storage.customOptions) { // If custom settings are defined
     storage.customOptions.forEach((element) => {
       var elurl = new URL(element.url);
-      if (elurl.host === url) {
+      if (elurl.host === url) { // Compare custom setting's hostname with the tab's
         injected = true;
-        if(element.shadowActivated) {
+        if(element.shadowActivated) { // If the settings has text shadow activated
           css = '::selection { background: ' + element.background + ' !important; color: ' + element.color + ' !important; text-shadow: ' + element.shadowColor + ' 0px 0px ' + element.shadowBlur + 'px !important}';
         } else {
           css = '::selection { background: ' + element.background + ' !important; color: ' + element.color + ' !important; text-shadow: none !important}';
@@ -44,8 +44,8 @@ async function restoreOptions(tab) {
       }
     });
   }
-  if (!injected && storage.witness) {
-    if(storage.shadowActivated) {
+  if (!injected && storage.witness) { // If url didn't match any custom settings and the user already defined some settings (avoid injecting undefined values into CSS)
+    if(storage.shadowActivated) { // If the settings has text shadow activated
       css = '::selection { background: ' + storage.background_color + ' !important; color: ' + storage.color + ' !important; text-shadow: ' + storage.shadowColor + ' 0px 0px ' + storage.shadowBlur + 'px !important}';
     } else {
       css = '::selection { background: ' + storage.background_color + ' !important; color: ' + storage.color + ' !important; text-shadow: none !important}';
@@ -57,15 +57,15 @@ async function restoreOptions(tab) {
       css: css
     });
   }
-  localstorage[tab.id] = css;
+  localstorage[tab.id] = css; // Store the injected CSS into local storage so that we can remove it later
 }
 
 async function update_action_icon(tabin) {
-  var tab = await browser.tabs.get(tabin.tabId);
-  var storage = await browser.storage.local.get();
-  if (/^((chrome|about):\/\/.*|$)/.test(tab.url)) {
-    browser.action.setIcon({path: browser.runtime.getURL('./icondisabled.png')});
-    return;
+  var tab = await browser.tabs.get(tabin.tabId); // Get the tab from the id
+  var storage = await browser.storage.local.get(); // Get settings
+  if (/^((chrome|about):\/\/.*|$|https:\/\/chrome\.google\.com\/webstore.*)/.test(tab.url)) { // If tab is on chrome://, about:// or on the chrome web store
+    browser.action.setIcon({path: browser.runtime.getURL('./icondisabled.png')}); // Set the icon to disabled (grey)
+    return; // Abort
   }
   try {
     var taburl = new URL(tab.url);
@@ -73,42 +73,35 @@ async function update_action_icon(tabin) {
     return;
   }
   var injected = false;
-  if (storage.customOptions) {
-    storage.customOptions.forEach((element) => {
+  if (storage.customOptions) { // If custom settings are defined
+    storage.customOptions.forEach((element) => { // For each custom setting
       var elurl = new URL(element.url);
-      if (elurl.host === taburl.host) {
+      if (elurl.host === taburl.host) { // If the custom setting's url matches the hostname
         injected = true;
-        browser.action.setIcon({path: browser.runtime.getURL('./iconcustom.png')});
+        browser.action.setIcon({path: browser.runtime.getURL('./iconcustom.png')}); // Set to custom icon (yellow)
       }
     });
   }
-  if (!injected) {
-    browser.action.setIcon({path: browser.runtime.getURL('./icon.png')});
+  if (!injected) { // If the default settings are applied
+    browser.action.setIcon({path: browser.runtime.getURL('./icon.png')}); // Set to the default icon (blue)
   }
 }
 
-browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  restoreOptions(tab);
-  update_action_icon({tabId: tab.id});
+browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {// When a tab is updated
+  restoreOptions(tab); // Inject CSS
+  update_action_icon({tabId: tab.id}); // Update icon
 });
 
 browser.runtime.onMessage.addListener((message, sender) => {
   switch(message.request) {
-      case "inject-css-all":
+      case "inject-css-all": // When the user saves the settings (from popup)
         browser.tabs.query({}).then((result) => {
           tabids = result.forEach((tab) => {
-            restoreOptions(tab);
+            restoreOptions(tab); // Apply new settings in each tab
           });
         });
         break;
       }
     });
     
-browser.tabs.onActivated.addListener(update_action_icon);
-
-// function onIconClicked() {
-// 	browser.tabs.create({
-// 		url: browser.extension.getURL('./popup.html'),
-// 	});
-// };
-// browser.browserAction.onClicked.addListener(onIconClicked);
+browser.tabs.onActivated.addListener(update_action_icon); // When the active tab has changed: update icon
