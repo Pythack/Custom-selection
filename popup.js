7a3ff1aaf60490841ae7dd82e3846f555552618b
Pythack <59161870+Pythack@users.noreply.github.com>
2023-01-04 10:57:17 +0100

    Add comments

--- popup.js
+++ popup.js
@@ -5,6 +5,10 @@ if (typeof browser === "undefined") {
 function saveSuccess(item){browser.notifications.create("notification-save-status",{type:'basic',title:'Selection styler: Saving success',message:"Preferences saved successfully. ",iconUrl:"./images/iconvalid.png"});setTimeout(function(){browser.notifications.clear("notification-save-status");},1000);};
 function saveError(item){browser.notifications.create("notification-save-status",{type:'basic',title:'Selection styler: Saving error',message:"Error saving preferences",iconUrl:"./images/iconfail.png"});setTimeout(function(){browser.notifications.clear("notification-save-status");},1000);};
 
+function onError(error) {
+	console.log(`Error:${error}`);
+}
+
 class Custom_option {
 	constructor(url, background, color, shadowActivated, shadowColor, shadowBlur) {
 		this.url = url;
@@ -17,47 +21,38 @@ class Custom_option {
 };
 
 function addCustom() {
-	function continueCustom(result) {
-		var custom_url = document.querySelector("#add_url").value;
-		var customSettings = new Custom_option(custom_url, document.querySelector("#background_color").value || "#007ef3", document.querySelector("#color").value || "white", document.querySelector("input#activate_textShadow").checked || false, document.querySelector("#shadow-color").value || "none", document.querySelector("#shadow-blur").value || "0");
-		var customs = result.customOptions || [];
-		customs.push(customSettings);
-		browser.storage.local.set({
+	let getting = browser.storage.local.get(); // Get storage
+	getting.then(result => {
+		var custom_url = document.querySelector("#add_url").value; // Get new custom url
+		var customSettings = new Custom_option(custom_url, document.querySelector("#background_color").value || "#007ef3", document.querySelector("#color").value || "white", document.querySelector("input#activate_textShadow").checked || false, document.querySelector("#shadow-color").value || "none", document.querySelector("#shadow-blur").value || "0"); // Create new element with currently displayed settings
+		var customs = result.customOptions || []; // Get current saved settings; if there are none, initialize an empty list
+		customs.push(customSettings); // Add new custom settings to the list
+		browser.storage.local.set({ // Store the new list in the storage
 			customOptions: customs
 		});
-		var optionToAdd = document.createElement("option");
-		optionToAdd.textContent = custom_url;
-		optionToAdd.value = custom_url;
+		var optionToAdd = document.createElement("option"); // Create new option element
+		optionToAdd.textContent = custom_url; // Add custom url to option
+		optionToAdd.value = custom_url; // Add custom url to option value
 		var select = document.querySelector("#custom_select");
-		select.appendChild(optionToAdd);
-	};
-
-	function onError(error) {
-		console.log(`Error:${error}`);
-	};
-	let getting = browser.storage.local.get();
-	getting.then(continueCustom, onError);
+		select.appendChild(optionToAdd); // Append new option to select
+	}, onError);
 };
 
 function removeCustom() {
-	function continueCustom(result) {
-		var selectIndex = document.querySelector("#custom_select").selectedIndex - 1;
-		var customs = result.customOptions;
-		customs.splice(selectIndex, 1);
-		browser.storage.local.set({
+	let getting = browser.storage.local.get(); // Get storage
+	getting.then(result => {
+		var selectIndex = document.querySelector("#custom_select").selectedIndex - 1; // Get selected index (-1 to ignore first index: default settings)
+		var customs = result.customOptions; // Get stored custom settings
+		customs.splice(selectIndex, 1); // Remove custom setting from list
+		browser.storage.local.set({ // Store new list in storage
 			customOptions: customs
 		});
-		var optionToRemove = document.querySelector("#custom_select");
-		optionToRemove.remove(document.querySelector("#custom_select").selectedIndex);
+		var customSettingsSelect = document.querySelector("#custom_select"); // Get option in select
+		customSettingsSelect.remove(document.querySelector("#custom_select").selectedIndex); // Remove option from select
 		var selectTrueIndex = document.querySelector("#custom_select").selectedIndex;
-		if(selectTrueIndex != 0) {
-			document.querySelector("#url_div").style.display = "block";
-			document.querySelector("#url_div").value = document.querySelector("#custom_select").value;
-			document.querySelector("#remove_custom").style.display = "block";
-		} else {
-			document.querySelector("#url_div").style.display = "none";
-			document.querySelector("#remove_custom").style.display = "none";
-		}
+		document.querySelector("#url_div").style.display = "none"; // Hide url edition input
+		document.querySelector("#remove_custom").style.display = "none"; // Hide remove button
+		// Restore default settings in the inputs
 		document.querySelector("#background_color").jscolor.fromString(result.background_color || "#007ef3");
 		document.querySelector("#color").jscolor.fromString(result.color || "white");
 		document.querySelector("#shadow-color").jscolor.fromString(result.shadowColor || "#ffffff");
@@ -68,27 +63,15 @@ function removeCustom() {
 		} else {
 			document.querySelector('div#textShadowOptions').style.display = "none";
 		}
-		document.querySelector("#preview").style.background = document.querySelector("#background_color").value;
-		document.querySelector("#preview").style.color = document.querySelector("#color").value;
-		if (document.querySelector("input#activate_textShadow").checked) {
-			document.querySelector("#preview").style.textShadow = document.querySelector("#shadow-color").value + " 0px 0px " + document.querySelector("#shadow-blur").value + "px";
-		} else {
-			document.querySelector("#preview").style.textShadow = "";
-		}
-	};
-
-	function onError(error) {
-		console.log(`Error:${error}`);
-	};
-	let getting = browser.storage.local.get();
-	getting.then(continueCustom, onError);
+		updatePreview(); // Update preview
+	}, onError);
 };
 
-function saveOptions(e) {
+function saveOptions(e) { // Function called when user clicks on "Save" button
 	e.preventDefault();
-	var selectIndex = document.querySelector("#custom_select").selectedIndex;
-	if(selectIndex == 0) {
-		var preferencesSave = browser.storage.local.set({
+	var selectIndex = document.querySelector("#custom_select").selectedIndex; // Get selected custom option
+	if(selectIndex == 0) { // If 0, then user selected default settings
+		var preferencesSave = browser.storage.local.set({ // Update default settings
 			background_color: document.querySelector("#background_color").value || "#007ef3",
 			color: document.querySelector("#color").value || "white",
 			shadowActivated: document.querySelector("input#activate_textShadow").checked || false,
@@ -96,12 +79,13 @@ function saveOptions(e) {
 			shadowBlur: document.querySelector("#shadow-blur").value || "0",
 			witness: true
 		});
-		preferencesSave.then(saveSuccess, saveError);
+		preferencesSave.then(saveSuccess, saveError); // Display notifications
 	} else {
-		function continueCustom(result) {
-			var selectIndex = document.querySelector("#custom_select").selectedIndex - 1;
-			var customs = result.customOptions;
-			customs[selectIndex] = {
+		let getting = browser.storage.local.get();
+		getting.then(result => {
+			var selectIndex = document.querySelector("#custom_select").selectedIndex - 1; // Get index (-1 to ignore first index: default settings)
+			var customs = result.customOptions; // Get current custom settings
+			customs[selectIndex] = { // Update selected custom settings
 				background: document.querySelector("#background_color").value || "#007ef3",
 				color: document.querySelector("#color").value || "white",
 				shadowActivated: document.querySelector("input#activate_textShadow").checked || false,
@@ -109,35 +93,31 @@ function saveOptions(e) {
 				shadowBlur: document.querySelector("#shadow-blur").value || "0",
 				url: document.querySelector("#change_url").value || "auto"
 			};
-			var preferencesSave = browser.storage.local.set({
+			var preferencesSave = browser.storage.local.set({ // Update custom settings
 				customOptions: customs
 			});
-			preferencesSave.then(saveSuccess, saveError);
-		};
-
-		function onError(error) {
-			console.log(`Error:${error}`);
-		}
-		let getting = browser.storage.local.get();
-		getting.then(continueCustom, onError);
+			preferencesSave.then(saveSuccess, saveError); // Display notifications
+		}, onError);
 	};
-	browser.runtime.sendMessage({
+	browser.runtime.sendMessage({ // Send message to background script asking to inject new CSS in all tabs
 		request: "inject-css-all"
 	});
 };
-function updatePreview() {
-  document.querySelector("#preview").style.background = document.querySelector("#background_color").value;
-  document.querySelector("#preview").style.color = document.querySelector("#color").value;
-  if (document.querySelector("input#activate_textShadow").checked) {
-    document.querySelector("#preview").style.textShadow = document.querySelector("#shadow-color").value + " 0px 0px " + document.querySelector("#shadow-blur").value + "px";
-  } else {
-    document.querySelector("#preview").style.textShadow = "";
-  }
+function updatePreview() { // Function called when input values are changed
+	// Apply selection style to preview style
+	document.querySelector("#preview").style.background = document.querySelector("#background_color").value;
+	document.querySelector("#preview").style.color = document.querySelector("#color").value;
+	if (document.querySelector("input#activate_textShadow").checked) {
+		document.querySelector("#preview").style.textShadow = document.querySelector("#shadow-color").value + " 0px 0px " + document.querySelector("#shadow-blur").value + "px";
+	} else {
+		document.querySelector("#preview").style.textShadow = "";
+	}
 };
 
 function restoreOptions() {
-
-  function setCurrentChoice(result) {
+  let getting = browser.storage.local.get(); // Get storage
+  getting.then(result => {
+	// Restore values to fields
     document.querySelector("#background_color").jscolor.fromString(result.background_color || "#00EF380");
     document.querySelector("#color").jscolor.fromString(result.color || "007EF3FF");
     document.querySelector("#shadow-color").jscolor.fromString(result.shadowColor || "#FFFFFF00");
@@ -146,8 +126,8 @@ function restoreOptions() {
     if (result.shadowActivated) {
       document.querySelector('div#textShadowOptions').style.display = "block";
     };
-	if (result.customOptions) {
-		result.customOptions.forEach(element => {
+	if (result.customOptions) { // If there are custom settings
+		result.customOptions.forEach(element => { // For each custom setting: add option to select
 		var option = document.createElement("option");
 		option.textContent = element.url;
 		option.value = element.url;
@@ -155,38 +135,22 @@ function restoreOptions() {
 		select.appendChild(option);
 		});
 	}
-  };
-
-  function onError(error) {
-    console.log(`Error: ${error}`);
-  };
-
-  function updatePreview() {
-    document.querySelector("#preview").style.background = document.querySelector("#background_color").value;
-    document.querySelector("#preview").style.color = document.querySelector("#color").value;
-    if (document.querySelector("input#activate_textShadow").checked) {
-      document.querySelector("#preview").style.textShadow = document.querySelector("#shadow-color").value + " 0px 0px " + document.querySelector("#shadow-blur").value + "px";
-    } else {
-      document.querySelector("#preview").style.textShadow = "";
-    };
-  };
-
-  let getting = browser.storage.local.get();
-  getting.then(setCurrentChoice, onError);
-  updatePreview();
+  }, onError);
+  updatePreview(); // Update preview
 };
 
 
 function updateShadowColorDisplay(){if(document.querySelector("input#activate_textShadow").checked){document.querySelector('div#textShadowOptions').style.display="block"}else{document.querySelector('div#textShadowOptions').style.display="none"}};
-function changeCustomDisplay() {
-	var selectIndex = document.querySelector("#custom_select").selectedIndex;
-	if(selectIndex != 0) {
-		document.querySelector("#url_div").style.display = "block";
-		document.querySelector("#change_url").value = document.querySelector("#custom_select").value;
-		document.querySelector("#remove_custom").style.display = "block";
-
-		function setCurrentChoice(result) {
-			var customs = result.customOptions;
+function changeCustomDisplay() { // Function called when select changes
+	var selectIndex = document.querySelector("#custom_select").selectedIndex; // Get selected index
+	if(selectIndex != 0) { // If selected option is not default settings
+		document.querySelector("#change_url").value = document.querySelector("#custom_select").value; // Initialize url edition input to selected setting's url
+		document.querySelector("#url_div").style.display = "block"; // Display custom url edition div
+		document.querySelector("#remove_custom").style.display = "block"; // Display remove button
+		let getting = browser.storage.local.get(); // Get storage
+		getting.then(result => {
+			var customs = result.customOptions; // Get custom settings
+			// Restore settings in inputs
 			document.querySelector("#background_color").jscolor.fromString(customs[selectIndex - 1].background || "#007ef3");
 			document.querySelector("#color").jscolor.fromString(customs[selectIndex - 1].color || "white");
 			document.querySelector("#shadow-color").jscolor.fromString(customs[selectIndex - 1].shadowColor || "#ffffff");
@@ -197,25 +161,14 @@ function changeCustomDisplay() {
 			} else {
 				document.querySelector('div#textShadowOptions').style.display = "none";
 			};
-			document.querySelector("#preview").style.background = document.querySelector("#background_color").value;
-			document.querySelector("#preview").style.color = document.querySelector("#color").value;
-			if (document.querySelector("input#activate_textShadow").checked) {
-				document.querySelector("#preview").style.textShadow = document.querySelector("#shadow-color").value + " 0px 0px " + document.querySelector("#shadow-blur").value + "px";
-			} else {
-				document.querySelector("#preview").style.textShadow = "";
-			};
-		};
-
-		function onError(error) {
-			console.log(`Error:${error}`);
-		};
-		let getting = browser.storage.local.get();
-		getting.then(setCurrentChoice, onError);
+			updatePreview(); // Update preview
+		}, onError);
 	} else {
-		document.querySelector("#url_div").style.display = "none";
-		document.querySelector("#remove_custom").style.display = "none";
-
-		function setCurrentChoice(result) {
+		document.querySelector("#url_div").style.display = "none"; // Hide custom url edition div
+		document.querySelector("#remove_custom").style.display = "none"; // Hide remove button
+		let getting = browser.storage.local.get(); // Get storage
+		getting.then(result => {
+			// Restore settings in inputs
 			document.querySelector("#background_color").jscolor.fromString(result.background_color || "#007ef3");
 			document.querySelector("#color").jscolor.fromString(result.color || "white");
 			document.querySelector("#shadow-color").jscolor.fromString(result.shadowColor || "#ffffff");
@@ -226,22 +179,12 @@ function changeCustomDisplay() {
 			} else {
 				document.querySelector('div#textShadowOptions').style.display = "none";
 			}
-			document.querySelector("#preview").style.background = document.querySelector("#background_color").value;
-			document.querySelector("#preview").style.color = document.querySelector("#color").value;
-			if (document.querySelector("input#activate_textShadow").checked) {
-				document.querySelector("#preview").style.textShadow = document.querySelector("#shadow-color").value + " 0px 0px " + document.querySelector("#shadow-blur").value + "px";
-			} else {
-				document.querySelector("#preview").style.textShadow = "";
-			};
-		};
-
-		function onError(error) {
-			console.log(`Error:${error}`);
-		};
-		let getting = browser.storage.local.get();
-		getting.then(setCurrentChoice, onError);
+			updatePreview(); // Update preview
+		}, onError);
 	};
 };
+
+// Set event listeners
 document.addEventListener("DOMContentLoaded", restoreOptions);
 document.addEventListener("DOMContentLoaded", () => {setTimeout(updatePreview, 100)});
 document.querySelector("#save_btn").addEventListener("click", saveOptions);
@@ -252,7 +195,3 @@ document.querySelector("#remove_custom").addEventListener("click", removeCustom)
 document.querySelector("input#activate_textShadow").addEventListener("change", updatePreview);
 document.querySelector("select#custom_select").addEventListener("change", changeCustomDisplay);
 document.querySelector("input#activate_textShadow").addEventListener("change", updateShadowColorDisplay);
-// document.querySelector("#promotionaldiv").addEventListener("click", (event) => {document.querySelector("#promotionaldiv").style.display = "none"})
-/*browser.tabs.create({
-    url:browser.extension.getURL('./popup.html')
-});*/
